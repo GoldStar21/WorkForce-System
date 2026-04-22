35943a4ce144f89e81cd9cdd8a27b3ae28272b09
GoldStar21 <filip.buzuk21@gmail.com>
2026-04-22 14:33:18 +0200

    Responsiveness

--- frontend/lib/api_config.ts
+++ frontend/lib/api_config.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 */
 
 const api_config = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
   withCredentials: true,
 });
 
